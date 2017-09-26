58cddd7b2817e7a740519c1fb0073ac7a95bbca9
Kim JinSeok <jskim2code@gmail.com>
2017-09-26 12:10:59 +0900

    blog/view/post_detail

--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -1,15 +1,33 @@
 from django.shortcuts import render
 
-
 # Create your views here.
 from blog.models import Post
 
 
 def post_list(request):
     # post_list view 가 published_date 이 존재하는 Post 목록만 보여주도록 수정
-    posts = Post.objects.filter(publisted_date__isnull=False)
+    # posts = Post.objects.filter(published_date__isnull=False)
+    posts = Post.objects.all()
     context = {
         # posts key의 value 는 QuerySet
         'posts': posts,
     }
     return render(request, 'blog/post_list.html', context)
+
+
+def post_detail(request):
+    # post_detail기능을 구현하는 함수 구현
+    # 'post'라는 key로 Post.objects.first()에 해당하는 Post객체를 전달
+    # 템플릿은 'blog/post_detail.html'이용
+    # Post 인스턴스 1개만 가져옴, 변수명은 posts가 아닌 단일 객체를 나타내는 post를 사용
+    post = Post.objects.first()
+    context = {
+        'post': post,
+    }
+    return render(request, 'blog/post_detail.html', context)
+
+    # 실제 템플릿 생성
+    # 'post'라는 변수를 이용해 Post 객체의 내용을 출력
+
+    # UrlResolver(urls.py)
+    # /post/detail/ --> url을 'post_detail'뷰와 연결
--- djangogirls/config/urls.py
+++ djangogirls/config/urls.py
@@ -21,4 +21,5 @@ from blog.views import post_list
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', post_list),
+    url(r'^/post/detail/$', post_detail),
 ]
